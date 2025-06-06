035782451f044dfbed98620d1fc45a83767a6f79
Yuly <yulyalvarezb@gmail.com>
2025-06-05 23:09:19 -0500

    autenticación con Firebase y lógica del login

--- app/src/main/java/com/sena/saferesidenceapp/MainActivity.java
+++ app/src/main/java/com/sena/saferesidenceapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.sena.saferesidenceapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -7,13 +8,15 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,17 +44,38 @@ public class MainActivity extends AppCompatActivity {
                 password = String.valueOf(editTextPassword.getText());
 
                 //Condicional para verificar entrada de información
-                if (TextUtils.isEmpty(email)){
+                if (TextUtils.isEmpty(email)) {
                     Toast.makeText(MainActivity.this, "Ingrese correo", Toast.LENGTH_SHORT).show();
                     return;
                 }
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(MainActivity.this, "Ingrese contraseña", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                firebaseAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(MainActivity.this, "Ingreso correcto", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(MainActivity.this, Dashboard.class);
+                                    startActivity(intent);
+                                    finish();
+                                } else {
+                                    Toast.makeText(MainActivity.this, "Error al iniciar sesión", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
             }
         }));
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        /**
+         * ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+         *             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+         *             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+         *             return insets;
+         *         });
+         */
+
     }
 }
